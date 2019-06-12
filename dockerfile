739c9f1c533d08d682fe18af044022bde4165bf5
Jessie Leung <jleu1656@gmail.com>
2019-06-12 22:13:09 +1000

    Test

--- dockerfile
+++ dockerfile
@@ -1,6 +1,4 @@
 FROM marcelocorreia/terraform
 WORKDIR /infra
 COPY . /infra
-RUN ["./create-aws-credentials-file.sh"]
-RUN ["ls"]
 ENTRYPOINT ["./provision-infrastructure.sh"]
--- provision-infrastructure.sh
+++ provision-infrastructure.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+echo "Storing credentials..."
+
+mkdir -p /aws
+
+cat > /aws/credentials << EOF
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOF
+
 echo "Provisioning infrastructure..."
 
 terraform init
