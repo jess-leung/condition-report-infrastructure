4a313c83a25c3ea05c7eecb3716e0491b6285245
Jessie Leung <jleu1656@gmail.com>
2019-06-12 21:41:28 +1000

    Test create s3 bucket

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ provider "aws" {
   region     = "us-east-1"
   shared_credentials_file = "~/.aws/creds"
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
