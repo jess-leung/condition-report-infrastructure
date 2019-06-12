c76f9ed127f0768c728cbd3713e73f1fa35ae1be
Jessie Leung <jleu1656@gmail.com>
2019-06-12 21:55:30 +1000

    Rewrite directories

--- create-aws-credentials-file.sh
+++ create-aws-credentials-file.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.aws
+echo "Storing credentials..."
 
-cat > ~/.aws/credentials << EOL
+mkdir -p /.aws
+
+cat > /.aws/credentials << EOL
 [default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOL
 
-ls
-pwd
+ls /.aws
--- dockerfile
+++ dockerfile
@@ -2,4 +2,5 @@ FROM marcelocorreia/terraform
 WORKDIR /infra
 COPY . /infra
 RUN ["./create-aws-credentials-file.sh"]
+RUN ["ls"]
 ENTRYPOINT ["./provision-infrastructure.sh"]
