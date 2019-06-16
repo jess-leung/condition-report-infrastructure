00dd766314502861dccad0d296674b503965ff59
Jessie Leung <jleu1656@gmail.com>
2019-06-16 19:45:15 +1000

    Use env variables for AWS creds

--- provision-infrastructure.sh
+++ provision-infrastructure.sh
@@ -2,11 +2,11 @@
 
 echo "Storing credentials..."
 
-mkdir -p /.aws
-
-echo "[default]" >> /.aws/credentials
-echo "aws_access_key_id=${AWS_SECRET_ACCESS_KEY}" >> /.aws/credentials
-echo "aws_secret_access_key=${AWS_ACCESS_KEY_ID}" >> /.aws/credentials
+#mkdir -p /.aws
+#
+#echo "[default]" >> /.aws/credentials
+#echo "aws_access_key_id=${AWS_SECRET_ACCESS_KEY}" >> /.aws/credentials
+#echo "aws_secret_access_key=${AWS_ACCESS_KEY_ID}" >> /.aws/credentials
 
 echo "Provisioning infrastructure..."
 
