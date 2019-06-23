1d30ba8efd180c7b40160e8a6bf1f484ef40a893
Jessie Leung <jleu1656@gmail.com>
2019-06-23 20:59:08 +1000

    Create eb env

--- main.tf
+++ main.tf
@@ -2,12 +2,13 @@ provider "aws" {
   region     = "us-west-1"
 }
 
-resource "aws_s3_bucket" "condition-report-tester-bucket" {
-  bucket = "condition-report-tester-bucket"
-  acl    = "private"
+resource "aws_elastic_beanstalk_application" "condition-report-service" {
+  name        = "condition-report-service"
+  description = "Elastic beanstalk app for condition report service"
+}
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+resource "aws_elastic_beanstalk_environment" "tfenvtest" {
+  name                = "condition-report-service"
+  application         = "${aws_elastic_beanstalk_application.condition-report-service.name}"
+  solution_stack_name = "Single Container Docker 18.06 version 2.12.14"
 }
--- provision-infrastructure.sh
+++ provision-infrastructure.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Storing credentials..."
-
-if [ -z "$AWS_ACCESS_KEY_ID" ]
-then
-      echo "\$var is empty"
-else
-      echo "\$var is NOT empty"
-fi
-
-#mkdir -p /.aws
-#
-#echo "[default]" >> /.aws/credentials
-#echo "aws_access_key_id=${AWS_SECRET_ACCESS_KEY}" >> /.aws/credentials
-#echo "aws_secret_access_key=${AWS_ACCESS_KEY_ID}" >> /.aws/credentials
-
 echo "Provisioning infrastructure..."
 
 terraform init
