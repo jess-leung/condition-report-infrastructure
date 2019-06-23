c41ca80580043b21e55d1ad4461fdc120d77e3fc
Jessie Leung <jleu1656@gmail.com>
2019-06-23 21:16:51 +1000

    Change eb name

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 resource "aws_elastic_beanstalk_application" "condition-report-service" {
-  name        = "condition-report-service"
+  name        = "condition-report-service-eb"
   description = "Elastic beanstalk app for condition report service"
 }
 
 resource "aws_elastic_beanstalk_environment" "tfenvtest" {
-  name                = "condition-report-service"
+  name                = "condition-report-service-eb"
   application         = "${aws_elastic_beanstalk_application.condition-report-service.name}"
   solution_stack_name = "64bit Amazon Linux 2018.03 v2.12.14 running Docker 18.06.1-ce"
 }
