955ad532a9c523245cbe737e42f97dee4d9de3f6
Jessie Leung <jleu1656@gmail.com>
2019-06-23 21:10:14 +1000

    Change solution stack

--- main.tf
+++ main.tf
@@ -10,5 +10,5 @@ resource "aws_elastic_beanstalk_application" "condition-report-service" {
 resource "aws_elastic_beanstalk_environment" "tfenvtest" {
   name                = "condition-report-service"
   application         = "${aws_elastic_beanstalk_application.condition-report-service.name}"
-  solution_stack_name = "Single Container Docker 18.06 version 2.12.14"
+  solution_stack_name = "64bit Amazon Linux 2018.03 v2.12.14 running Docker 18.06.1-ce"
 }
