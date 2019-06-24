42c9987d02305cb905f4873f778c383d8b1cae33
Jessie Leung <jleu1656@gmail.com>
2019-06-24 19:41:29 +1000

    Try create beanstalk env again

--- main.tf
+++ main.tf
@@ -1,5 +1,34 @@
 provider "aws" {
-  region     = "us-west-1"
+  region     = "ap-southeast-2"
+}
+
+# Create a VPC to launch our instances into
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create an internet gateway to give our subnet access to the outside world
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.default.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.default.id}"
+}
+
+# Create a subnet for the ELB & EC2 intances
+resource "aws_subnet" "default" {
+  vpc_id                  = "${aws_vpc.default.id}"
+  availability_zone       = "eu-west-1a"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags {
+    Name = "Subnet A"
+  }
 }
 
 resource "aws_elastic_beanstalk_application" "condition-report-service" {
@@ -7,8 +36,21 @@ resource "aws_elastic_beanstalk_application" "condition-report-service" {
   description = "Elastic beanstalk app for condition report service"
 }
 
-resource "aws_elastic_beanstalk_environment" "tfenvtest" {
+resource "aws_elastic_beanstalk_environment" "condition-report-service-test-env" {
   name                = "condition-report-service-eb"
   application         = "${aws_elastic_beanstalk_application.condition-report-service.name}"
   solution_stack_name = "64bit Amazon Linux 2018.03 v2.12.14 running Docker 18.06.1-ce"
+  tier                  = "WebServer"
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = "${aws_vpc.default.id}"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = "${aws_subnet.default.id}"
+  }
 }
