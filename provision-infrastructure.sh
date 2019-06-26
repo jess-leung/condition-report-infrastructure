afdd0243bbee7a1859bb2d71bf66f1697ea39f81
Jessie Leung <jleu1656@gmail.com>
2019-06-26 23:34:04 +1000

    Change dir

--- provision-infrastructure.sh
+++ provision-infrastructure.sh
@@ -3,6 +3,7 @@
 echo "Provisioning infrastructure..."
 
 terraform init
-terraform plan
-terraform apply -auto-approve main.tf
-terraform apply -auto-approve rds/rds.tf
+terraform plan .
+terraform plan rds
+terraform apply -auto-approve .
+terraform apply -auto-approve rds
